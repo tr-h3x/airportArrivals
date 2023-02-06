bdca71735b1d5e2e6fda9d709e96b697491e31ca
tr_h3x <travis.hetzel@gmail.com>
2023-02-06 15:13:41 -0800

    inital upload

---
+++ airportSchedule/arrivals/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from datetime import date, timedelta
+import requests
+import os
+import json
+
+
+# AEROAPI_KEY = 'J1usNirxkMs8XSTY3EicZQBBEY5JI9Qe'
+AEROAPI_KEY = str(os.getenv('AEROAPI_KEY'))
+AEROAPI = requests.Session()
+AEROAPI.headers.update({"x-apikey": AEROAPI_KEY})
+
+def arrivals(request):
+
+    if request.method == "POST":
+        airport = request.POST['airport']
+        today = date.today()
+        yesterday = today - timedelta(1)
+        tomorrow = today + timedelta(1)
+        results = AEROAPI.get(f"https://aeroapi.flightaware.com/aeroapi/airports/{airport}/flights/arrivals?start={today}&end={tomorrow}&max_pages=10")
+
+        schedule = results.json()
+
+        arrivals = schedule['arrivals']
+        
+        context = {
+            'arrivals': arrivals,
+            'airport': airport,
+        }
+
+        print(arrivals)
+    else: 
+        context = {}
+    return render(request, 'arrivals.html', context )
